698b98529fde28cd6fc701d3d1a82cf3912d07df
Jx <103049321+JxJxxJxJ@users.noreply.github.com>
2024-08-25 18:50:13 -0300

    idea de main para debugear

--- mybash.c
+++ mybash.c
@@ -16,24 +16,35 @@ static void show_prompt(void) {
 }
 
 int main(int argc, char *argv[]) {
-  pipeline pipe;
-  Parser input;
-  bool quit = false;
 
-  input = parser_new(stdin);
-  while (!quit) {
-    ping_pong_loop(NULL);
-    show_prompt();
-    pipe = parse_pipeline(input);
+  // ---------------------------------------------------------
+  // | Zona para debugear, codigo temporal                   |
+  // ---------------------------------------------------------
+  Parser parser = parser_new(stdin);
+  pipeline pipe = parse_pipeline(parser);
+  printf("El pipe parseado desde stdin es: \n %s\n", pipeline_to_string(pipe));
 
-    /* Hay que salir luego de ejecutar? */
-    quit = parser_at_eof(input);
-    /*
-     * COMPLETAR
-     *
-     */
-  }
-  parser_destroy(input);
-  input = NULL;
+  // ---------------------------------------------------------
+  // | Abajo esta el codigo que nos dieron                   |
+  // ---------------------------------------------------------
+  // pipeline pipe;
+  // Parser input;
+  // bool quit = false;
+
+  // input = parser_new(stdin);
+  // while (!quit) {
+  //   ping_pong_loop(NULL);
+  //   show_prompt();
+  //   pipe = parse_pipeline(input);
+
+  //   /* Hay que salir luego de ejecutar? */
+  //   quit = parser_at_eof(input);
+  //   /*
+  //    * COMPLETAR
+  //    *
+  //    */
+  // }
+  // parser_destroy(input);
+  // input = NULL;
   return EXIT_SUCCESS;
 }
