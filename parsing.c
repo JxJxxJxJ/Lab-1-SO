44c47939cfc3b158fa067097957831eab7fce5e9
Jx <103049321+JxJxxJxJ@users.noreply.github.com>
2024-08-29 19:40:17 -0300

    parsing anda...

--- parsing.c
+++ parsing.c
@@ -12,13 +12,10 @@ static scommand parse_scommand(Parser p) {
   assert(!parser_at_eof(p));
 
   // Tomo un parser p y debo devolver un scommand
-  // Step1: Creo un fucking scommand
   scommand sc = scommand_new(); // ALOCA MEMORIA
 
-  // Step2:
   // Leo los argumentos uno por uno y los pongo donde haya que ponerlos
   // en el objeto scommand
-
   arg_kind_t tipo_de_argumento;
   char *argumento = NULL;
 
@@ -41,9 +38,6 @@ static scommand parse_scommand(Parser p) {
         break;
       }
     }
-    // Para debugear
-    printf("Metí un %s, de tipo %u\n", argumento, tipo_de_argumento);
-    printf("El scommand construido es: $ %s\n", scommand_to_string(sc));
     parser_skip_blanks(p);
   }
   // Ensuro lo siguiente
@@ -54,8 +48,7 @@ static scommand parse_scommand(Parser p) {
   //  3- Si lo que se consumió es un scommand valido, el resultado contiene la
   //     estructura correspondiente.
   // assert(???);
-  printf("He llegado a retornar el sc: $ <%s>\n", scommand_to_string(sc));
-  return sc; // TODO: Nunca retorno el sc
+  return sc;
 }
 
 /*
@@ -81,51 +74,36 @@ pipeline parse_pipeline(Parser p) {
   pipeline result = pipeline_new();
   scommand cmd = NULL;
   bool error = false, another_pipe = true;
-
   cmd = parse_scommand(p);
   error = (cmd == NULL); // Comando invalido al empezar.
-
+  if (another_pipe && error) {
+    printf("Error: no input or output file.\n");
+    pipeline_destroy(result);
+    return NULL;
+  }
   while (another_pipe && !error) {
-    // Agrego el comando a la secuencia de la pipeline.
     pipeline_push_back(result, cmd);
-
-    // Verifico si existe una '|' en la entrada.
     parser_op_pipe(p, &another_pipe);
-
-    // En caso de existir una '|', seteo cmd y error para parsear el siguiente
-    // comando simple. Si no, salgo del bucle.
     if (another_pipe) {
-      cmd =
-          parse_scommand(p); // ¿Quizás sea necesario un 'parser_skip_blanks()'
-                             // para saltar al siguiente comando?
+      cmd = parse_scommand(p);
       error = (cmd == NULL);
     }
-
-    // Usando como ejemplo ls -l -a < entrada.txt > salida.txt | wc -l
-    // 1° iteración: result = ( [ (["ls","-l","-a"], salida.txt , entrada.txt)
-    // ], ... ) 2° iteración: result = ( [ (["ls","-l","-a"], salida.txt ,
-    // entrada.txt) , (["wc","-l"], NULL , NULL) ], ... )
   }
-
-  // Opcionalmente un OP_BACKGROUND al final.
-  // Verifica si la pipeline se ejecuta en primer o segundo plano.
   bool in_background;
   parser_op_background(p, &in_background);
   pipeline_set_wait(result, !in_background);
-
-  // result = ( [ (["ls","-l","-a"], salida.txt , entrada.txt) , (["wc","-l"],
-  // NULL , NULL) ], true )
-
-  // Tolerancia a espacios posteriores.
-  parser_skip_blanks(p);
-
-  // Consumir todo lo que hay inclusive el \n.
   bool trash;
   parser_garbage(p, &trash);
 
-  // Si hubo error, hacemos cleanup.
   if (error) {
     result = pipeline_destroy(result);
+    result = NULL;
+  }
+
+  // ...
+  if (pipeline_length(result) == 1 &&
+      scommand_length(pipeline_front(result)) == 0) {
+    pipeline_destroy(result);
     result = NULL;
   }
 
