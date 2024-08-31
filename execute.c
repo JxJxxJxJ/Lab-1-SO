a3b223707bd6e115d73b11404eb66c7d9f0334c2
GasparSaenzValiente <gaspar.saenz.valiente@mi.unc.edu.ar>
2024-08-31 14:16:27 -0300

    execute module created

---
+++ execute.c
@@ -0,0 +1,18 @@
+#include "execute.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+ * Ejecuta un pipeline, identificando comandos internos, forkeando, y
+ *   redirigiendo la entrada y salida. puede modificar `apipe' en el proceso
+ *   de ejecución.
+ *   apipe: pipeline a ejecutar
+ * Requires: apipe!=NULL
+ */
+void execute_pipeline(pipeline apipe){
+
+
+
+}
