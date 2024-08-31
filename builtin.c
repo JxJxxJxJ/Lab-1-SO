defb3da8036e53f7379c7a3941058af4784c78eb
Jx <103049321+JxJxxJxJ@users.noreply.github.com>
2024-08-31 15:24:12 -0300

    builtin comenzado, hay extensibilidad, y una pregunta

---
+++ builtin.c
@@ -0,0 +1,107 @@
+#include "builtin.h"
+#include "command.h"
+#include <assert.h>
+#include <glib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Enum de los comandos que mybash acepta
+// CMD_COUNT es la cantidad de comandos aceptados hasta ahora por la shell
+typedef enum { CMD_CD, CMD_HELP, CMD_EXIT, CMD_COUNT } Command;
+
+static void handle_cd(scommand sc) { printf("Estoy ejecutando cd"); }
+static void handle_help(scommand sc) { printf("Estoy ejecutando help"); }
+static void handle_exit(scommand sc) { printf("Estoy ejecutando exit"); }
+
+// Cada comando <comando> tendra una funcion void asociada <handle_comando> y
+// una descripcion de lo que hace en help
+// uso const para decir que los datos son solo de lectura
+typedef struct {
+  const char *name;          // Nombre del comando
+  void (*handler)(scommand); // Puntero a la funcion que maneja el comando
+  const char *help;          //  Descripcion del comando (--help)
+} CMD_ENTRY;
+
+// Una tabla (array) de CMD_ENTRY
+static CMD_ENTRY command_table[CMD_COUNT] = {
+    {"cd", handle_cd, "Cambiar de directorio"},
+    {"help", handle_help, "Nuestro proyecto de SO"},
+    {"exit", handle_exit, "Es para salir de la sesion"}};
+
+/*
+ * Indica si el comando alojado en `cmd` es un comando interno
+ *
+ * REQUIRES: cmd != NULL
+ *
+ */
+bool builtin_is_internal(scommand cmd) {
+  assert(cmd != NULL);
+  bool b = false;
+
+  // Tomo lo que el scommand tenia al frente (el ls, el wc, etc sin sus flags)
+  // y armo un gstring con eso
+  GString *gstr_scommand = g_string_new(scommand_front(cmd));
+
+  // Recorre la tabla de comandos, si encuentro uno que se llame igual devuelvo
+  // true
+  for (size_t i = 0; i < CMD_COUNT; ++i) {
+    // Podria comparar con strcmp, quiza lo mas minimalista sea mejor, pero
+    // mezclar glib.h con strings.h me da cosa
+    // con gstring debo convertir el .name del campo a un gstring primero
+    GString *gstr_command_table_name = g_string_new(command_table[i].name);
+    if (g_string_equal(gstr_scommand, gstr_command_table_name)) {
+      b = true;
+    }
+  }
+
+  return b;
+}
+
+static builtin_is_internal_con_strcmp(scommand sc) {
+  bool builtin_is_internal(scommand * cmd) {
+    assert(cmd != NULL);
+    bool b = false;
+
+    // Tomo lo que el scommand tenia al frente (el ls, el wc, etc sin sus flags)
+    // y armo un string con eso
+    const char *command_name = scommand_front(cmd);
+
+    // Recorre la tabla de comandos, si encuentro uno que se llame igual
+    // devuelvo true
+    for (size_t i = 0; i < CMD_COUNT; ++i) {
+      if (strcmp(command_name, command_table[i].name) == 0) {
+        b = true;
+      }
+    }
+
+    return b;
+  }
+}
+
+/*
+ * Indica si el pipeline tiene solo un elemento y si este se corresponde a un
+ * comando interno.
+ *
+ * REQUIRES: p != NULL
+ *
+ * ENSURES:
+ *
+ * builtin_alone(p) == pipeline_length(p) == 1 &&
+ *                     builtin_is_internal(pipeline_front(p))
+ *
+ *
+ */
+bool builtin_alone(pipeline p) {
+  bool b = false;
+
+  return b;
+}
+
+/*
+ * Ejecuta un comando interno
+ *
+ * REQUIRES: {builtin_is_internal(cmd)}
+ *
+ */
+void builtin_run(scommand cmd) {}
